58dc90c6ab31f03543ba13c03bac12f93078c519
Patricio <patricio.fernandez.florez@gmail.com>
2026-01-28 15:08:47 +0100

    Add wait-for-db script and use prisma db push

---
+++ server/wait-for-db.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+set -e
+
+host="$1"
+shift
+cmd="$@"
+
+until nc -z "$host" 5432; do
+  echo "Waiting for PostgreSQL at $host:5432..."
+  sleep 2
+done
+
+echo "PostgreSQL is ready!"
+exec $cmd
